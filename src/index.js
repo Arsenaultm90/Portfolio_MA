739a7af372d2bb8f95fcf6d8490be47398ee62fe
Matt Arsenault <Matthew.B.Arsenault@gmail.com>
2022-07-19 07:49:35 -0400

    Changed path to website

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<HashRouter base='/src/index.js'>
+	<React.StrictMode>
 		<App />
-	</HashRouter>
+	</React.StrictMode>
 );
