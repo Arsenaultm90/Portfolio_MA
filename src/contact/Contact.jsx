ac7ceef8e63b43244b7ff0301ff2e513c2018b17
Matt Arsenault <Matthew.B.Arsenault@gmail.com>
2022-07-20 08:16:49 -0400

    Added email response functionality to contact form using EmailJS

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import './contact.scss';
 import { contact, closeBtn } from '../images/index';
 
 function Contact({ winState, setWinState }) {
+	const formRef = useRef(null);
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				'service_6j9w2au',
+				'template_hs2efoh',
+				formRef.current,
+				'EsjtS1Lx1DPEJXoir'
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+		e.target.reset();
+	};
+
 	const handleClose = (e) => {
 		setWinState({ contact: false });
 	};
@@ -27,35 +51,43 @@ function Contact({ winState, setWinState }) {
 				<div className='input-container'>
 					<div className='contact-box'>
 						<div className='left'></div>
-						<div className='right'>
+						<form className='right' ref={formRef} onSubmit={sendEmail}>
 							<h4>Contact Me</h4>
 							<p>Phone: 902-432-4145 | Email: Matthew.B.Arsenault@gmail.com</p>
 							<input
 								type='text'
 								className='field'
 								placeholder='Name'
+								tabIndex={1}
+								name='name'
 								required
 							/>
 							<input
 								type='email'
 								className='field'
 								placeholder='Email'
+								tabIndex={2}
+								name='email'
 								required
 							/>
 							<input
 								type='text'
 								className='field'
 								placeholder='Phone(Optional)'
+								tabIndex={3}
+								name='phone'
 							/>
 							<textarea
 								className='field area'
 								placeholder='Message'
+								tabIndex={4}
+								name='message'
 								required
 							></textarea>
 							<button className='submit-btn' type='submit'>
 								Submit
 							</button>
-						</div>
+						</form>
 					</div>
 				</div>
 			</div>
